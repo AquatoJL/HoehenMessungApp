39b4d2a59f7ff2283bf6e0414854006d50e04188
AquatoJL <103616070+AquatoJL@users.noreply.github.com>
2026-01-26 13:22:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,43 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics.texture import Texture
-from kivy.uix.camera import Camera
 from kivy.lang import Builder
-from jnius import autoclass
-import numpy as np
-import cv2
+from kivy.uix.boxlayout import BoxLayout
+import time
+Builder.load_string('''
+<CameraClick>:
+    orientation: 'vertical'
+    Camera:
+        id: camera
+        resolution: (640, 480)
+        play: False
+    ToggleButton:
+        text: 'Play'
+        on_press: camera.play = not camera.play
+        size_hint_y: None
+        height: '48dp'
+    Button:
+        text: 'Capture'
+        size_hint_y: None
+        height: '48dp'
+        on_press: root.capture()
+''')
 
 
-CameraInfo = autoclass('android.hardware.Camera$CameraInfo')
-CAMERA_INDEX = {'front': CameraInfo.CAMERA_FACING_FRONT, 'back': CameraInfo.CAMERA_FACING_BACK}
-Builder.load_file("myapplayout.kv")
+class CameraClick(BoxLayout):
+    def capture(self):
+        '''
+        Function to capture the images and give them the names
+        according to their captured time and date.
+        '''
+        camera = self.ids['camera']
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        camera.export_to_png("IMG_{}.png".format(timestr))
+        print("Captured")
 
 
-class AndroidCamera(Camera):
-    resolution = (640, 480)
-    index = CAMERA_INDEX['back']
-    counter = 0
+class TestCamera(App):
 
-    def on_tex(self, *l):
-        if self._camera._buffer is None:
-            return None
-
-        super(AndroidCamera, self).on_tex(*l)
-        self.texture = Texture.create(size=np.flip(self.resolution), colorfmt='rgb')
-        frame = self.frame_from_buf()
-        self.frame_to_screen(frame)
-
-    def frame_from_buf(self):
-        w, h = self.resolution
-        frame = np.frombuffer(self._camera._buffer.tostring(), 'uint8').reshape((h + h // 2, w))
-        frame_bgr = cv2.cvtColor(frame, 93)
-        if self.index:
-            return np.flip(np.rot90(frame_bgr, 1), 1)
-        else:
-            return np.rot90(frame_bgr, 3)
-
-    def frame_to_screen(self, frame):
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        cv2.putText(frame_rgb, str(self.counter), (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-        self.counter += 1
-        flipped = np.flip(frame_rgb, 0)
-        buf = flipped.tobytes()
-        self.texture.blit_buffer(buf, colorfmt='rgb', bufferfmt='ubyte')
-
-
-class MyLayout(BoxLayout):
-    pass
-
-
-class MyApp(App):
     def build(self):
-        return MyLayout()
+        return CameraClick()
 
 
-if __name__ == '__main__':
-    MyApp().run()
+TestCamera().run()
