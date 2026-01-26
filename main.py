24bbe0afeb335da1f6192864bb9c6a312f0171aa
AquatoJL <103616070+AquatoJL@users.noreply.github.com>
2026-01-26 10:06:34 +0100

    Refactor camera app to use MyLayout and AndroidCamera

--- main.py
+++ main.py
@@ -1,24 +1,58 @@
 from kivy.app import App
-from kivy.uix.camera import Camera
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.graphics.texture import Texture
+from kivy.uix.camera import Camera
+from kivy.lang import Builder
+from jnius import autoclass
+import numpy as np
+import cv2
 
-class CameraApp(App):
+
+CameraInfo = autoclass('android.hardware.Camera$CameraInfo')
+CAMERA_INDEX = {'front': CameraInfo.CAMERA_FACING_FRONT, 'back': CameraInfo.CAMERA_FACING_BACK}
+Builder.load_file("main.kv")
+
+
+class AndroidCamera(Camera):
+    resolution = (640, 480)
+    index = CAMERA_INDEX['back']
+    counter = 0
+
+    def on_tex(self, *l):
+        if self._camera._buffer is None:
+            return None
+
+        super(AndroidCamera, self).on_tex(*l)
+        self.texture = Texture.create(size=np.flip(self.resolution), colorfmt='rgb')
+        frame = self.frame_from_buf()
+        self.frame_to_screen(frame)
+
+    def frame_from_buf(self):
+        w, h = self.resolution
+        frame = np.frombuffer(self._camera._buffer.tostring(), 'uint8').reshape((h + h // 2, w))
+        frame_bgr = cv2.cvtColor(frame, 93)
+        if self.index:
+            return np.flip(np.rot90(frame_bgr, 1), 1)
+        else:
+            return np.rot90(frame_bgr, 3)
+
+    def frame_to_screen(self, frame):
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        cv2.putText(frame_rgb, str(self.counter), (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        self.counter += 1
+        flipped = np.flip(frame_rgb, 0)
+        buf = flipped.tobytes()
+        self.texture.blit_buffer(buf, colorfmt='rgb', bufferfmt='ubyte')
+
+
+class MyLayout(BoxLayout):
+    pass
+
+
+class MyApp(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical')
-        self.camera = Camera(play=True)
-        self.camera.resolution = (640, 480)
-        
-        btn = Button(text="Foto machen", size_hint=(1, 0.2))
-        btn.bind(on_press=self.take_picture)
-        
-        layout.add_widget(self.camera)
-        layout.add_widget(btn)
-        return layout
+        return MyLayout()
 
-    def take_picture(self, *args):
-        self.camera.export_to_png("foto.png")
-        print("Foto gespeichert")
 
 if __name__ == '__main__':
-    CameraApp().run()
+    MyApp().run()
