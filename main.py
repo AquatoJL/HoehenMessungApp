087bdd106e19960fdc6d35d38138d083589fea4d
AquatoJL <103616070+AquatoJL@users.noreply.github.com>
2026-01-26 16:51:33 +0100

    XCamera

--- main.py
+++ main.py
@@ -1,43 +1,13 @@
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-import time
-Builder.load_string('''
-<CameraClick>:
-    orientation: 'vertical'
-    Camera:
-        id: camera
-        resolution: (640, 480)
-        play: False
-    ToggleButton:
-        text: 'Play'
-        on_press: camera.play = not camera.play
-        size_hint_y: None
-        height: '48dp'
-    Button:
-        text: 'Capture'
-        size_hint_y: None
-        height: '48dp'
-        on_press: root.capture()
-''')
+from kivy.garden.xcamera import XCamera
 
-
-class CameraClick(BoxLayout):
-    def capture(self):
-        '''
-        Function to capture the images and give them the names
-        according to their captured time and date.
-        '''
-        camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("IMG_{}.png".format(timestr))
-        print("Captured")
-
-
-class TestCamera(App):
-
+class CameraApp(App):
     def build(self):
-        return CameraClick()
+        layout = BoxLayout()
+        self.camera = XCamera(play=True)
+        layout.add_widget(self.camera)
+        return layout
 
-
-TestCamera().run()
+if __name__ == '__main__':
+    CameraApp().run()
