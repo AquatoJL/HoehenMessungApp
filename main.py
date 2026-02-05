3b1cdf8cb6ecddda0c420f8aa03dfa84012f3381
Lehmann <LehmanJo@herrenknecht.com>
2026-02-05 07:47:22 +0100

    Update main

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from kivymd.uix.floatlayout import MDFloatLayout
 from kivy.lang import Builder
 from kivy.utils import platform
 from kivy.properties import StringProperty
-
 from camera4kivy import Preview
 
 if platform == 'android':
@@ -13,13 +12,13 @@ if platform == 'android':
     from jnius import autoclass
     from android.runnable import run_on_ui_thread
     from android import mActivity
-    
+
     View = autoclass('android.view.View')
-    
+
     @run_on_ui_thread
     def set_fullscreen(instance, width, height):
         mActivity.getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_FULLSCREEN | 
+            View.SYSTEM_UI_FLAG_FULLSCREEN |
             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         )
@@ -37,32 +36,26 @@ class CameraScreen(BoxLayout):
         if hasattr(self, 'ids') and 'preview' in self.ids:
             self.ids.preview.connect_camera()
             self.ids.preview.allow_stretch = True
-    
+
     def on_leave(self):
         if hasattr(self, 'ids') and 'preview' in self.ids:
             self.ids.preview.disconnect_camera()
 
-
 class CameraApp(MDApp):
     def build(self):
-        #Window.fullscreen = True
         Window.orientation = 'landscape'
-        
         Builder.load_file('main.kv')
-        
         self.camera_screen = CameraScreen()
         return self.camera_screen
-    
+
     def capture_photo(self):
         self.root.ids.preview.capture_photo(location='shared', subdir='Photos')
         print("Foto gespeichert!")
-    
+
     def on_start(self):
         if platform == 'android':
-            # Vollbild-Modus aktivieren
             Window.bind(on_resize=set_fullscreen)
             set_fullscreen(None, Window.width, Window.height)
-            
             self.dont_gc = AndroidPermissions(self.start_camera)
         else:
             self.start_camera()
@@ -70,12 +63,11 @@ class CameraApp(MDApp):
     def on_stop(self):
         if hasattr(self.camera_screen, 'on_leave'):
             self.camera_screen.on_leave()
-    
+
     def start_camera(self):
         self.dont_gc = None
         if hasattr(self.camera_screen, 'on_enter'):
             self.camera_screen.on_enter()
-
 
 if __name__ == '__main__':
     CameraApp().run()
